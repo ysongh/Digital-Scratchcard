d9775e80910b52c71f6d45109a40d14ca549f60c
You Song Hou <yousonghou@gmail.com>
2022-09-29 23:48:54 -0400

    feat: integrate RainbowKit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
 import { useState } from 'react';
 import './App.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 import ScratchCards from './pages/ScratchCards';
+import { ALCHEMY_ID } from './config';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    alchemyProvider({ apiKey: ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <ScratchCards />
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+        <ScratchCards />
+      </RainbowKitProvider>
+    </WagmiConfig>
   )
 }
 
---
+++ src/components/RainboxKit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RainboxKit() {
+  return (
+    <div>RainboxKit</div>
+  )
+}
+
+export default RainboxKit;
