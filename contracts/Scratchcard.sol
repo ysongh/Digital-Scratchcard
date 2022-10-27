00254917c8554b9e6f2d5e5934bf80291dd72e52
You Song Hou <yousonghou@gmail.com>
2022-10-26 23:44:52 -0400

    feat: add 'playGame' function to the contract

--- contracts/Scratchcard.sol
+++ contracts/Scratchcard.sol
@@ -4,18 +4,26 @@ pragma solidity ^0.8.9;
 contract Scratchcard {
     uint public imageTotal = 0;
     mapping(uint => string) public imageList;
+    uint public numberOfPlays = 0;
 
     function addImage(string memory _imageURL) external payable {
         imageList[imageTotal] = _imageURL;
         imageTotal++;
     }
 
-    function fillScratchCard() external view returns (string[] memory) {
+    function playGame() external returns (string[] memory) {
+        string[] memory imageURLs = fillScratchCard();
+        numberOfPlays += 1;
+
+        return imageURLs;
+    }
+
+    function fillScratchCard() internal view returns (string[] memory) {
         string[] memory imageURLs = new string[](9);
 
         for(uint i = 0; i < 9; i++){
-            uint _randomNumber = uint(keccak256(abi.encode(block.timestamp, block.difficulty, msg.sender, i))) % imageTotal + 1;
-            imageURLs[i] = imageList[_randomNumber - 1];
+            uint _randomNumber = uint(keccak256(abi.encode(block.timestamp, block.difficulty, msg.sender, i))) % imageTotal;
+            imageURLs[i] = imageList[_randomNumber];
         }
 
         return imageURLs;
