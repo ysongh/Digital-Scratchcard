f4ab0b82f51bf681e1d66d35202db0675558ada9
You Song Hou <yousonghou@gmail.com>
2022-11-06 00:43:50 -0400

    refactor: change the background color for Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,19 +6,22 @@ import {
 
 function Navbar() {
   return (
-    <nav className="relative container mx-auto p-3">
-      <div className="flex items-center justify-between">
-        <div>
-          <Link className='mr-3' to="/">Digital Scratchcard</Link>
-          <Link className='mr-3' to="/">Home</Link>
-          <Link className='mr-3' to="/add-advertisement">Add Advertisement</Link>
-          <Link to="/scratchcard">Scratch Card</Link>
-        </div>
-        
-        <div className="flex">
-          <ConnectButton />
+    <nav className="relative bg-navbar p-3">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link className='mr-3' to="/">Digital Scratchcard</Link>
+            <Link className='mr-3' to="/">Home</Link>
+            <Link className='mr-3' to="/add-advertisement">Add Advertisement</Link>
+            <Link to="/scratchcard">Scratch Card</Link>
+          </div>
+          
+          <div className="flex">
+            <ConnectButton />
+          </div>
         </div>
       </div>
+    
     </nav>
   )
 }
