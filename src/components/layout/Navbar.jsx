318dacd60c6cdad40583c38613d40f64468582d0
You Song Hou <yousonghou@gmail.com>
2022-11-08 00:29:24 -0500

    feat: add a logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,13 +4,17 @@ import {
   ConnectButton
 } from '@rainbow-me/rainbowkit';
 
+import Logo from '../../assets/Logo.png'
+
 function Navbar() {
   return (
     <nav className="relative bg-navbar p-3">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <div>
-            <Link className='mr-3' to="/">Digital Scratchcard</Link>
+            <Link className='mr-10' to="/">
+              <img className='logo' src={Logo} alt="logo" />
+            </Link>
             <Link className='mr-3' to="/">Home</Link>
             <Link className='mr-3' to="/add-advertisement">Add Advertisement</Link>
             <Link to="/scratchcard">Scratch Card</Link>
